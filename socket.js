5e7b7bf3e59e7f971aa0ddd77fa524c748c3b01c
arcade15xyz <arcade15xyz@gmail.com>
2026-04-13 13:22:46 +0530

    feat: Improve token retrieval logic in socket authentication middleware

--- socket.js
+++ socket.js
@@ -26,9 +26,8 @@ module.exports = {
     io.use((socket, next) => {
       try {
         const token =
-          socket.handshake.auth?.token ||
-          socket.handshake.headers?.authorization?.replace("Bearer ", "");
-
+        socket.handshake.headers?.authorization?.replace("Bearer ", "") ||
+          socket.handshake.auth?.token ;
         if (!token) {
           return next(new Error("Unauthorized: No token provided"));
         }
